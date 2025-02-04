93e85951731d110edfef433536d1cf43d456cc7d
thuypt <thuypt@reactplus.com>
2025-02-04 15:59:41 +0700

    WAI-ARIA - Reading mode

---
+++ WAI-ARIA/tabs-manual.js
@@ -0,0 +1,157 @@
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *
+ *   File:   tabs-manual.js
+ *
+ *   Desc:   Tablist widget that implements ARIA Authoring Practices
+ */
+
+"use strict";
+
+class TabsManual {
+  constructor(groupNode) {
+    this.tablistNode = groupNode;
+
+    this.tabs = [];
+
+    this.firstTab = null;
+    this.lastTab = null;
+
+    this.tabs = Array.from(this.tablistNode.querySelectorAll("[role=tab]"));
+    this.tabpanels = [];
+
+    this.range = document.createRange()
+    this.sel = document.getSelection()
+    this.range.selectNode(document.body)
+
+
+    for (let i = 0; i < this.tabs.length; i += 1) {
+      const tab = this.tabs[i];
+      const tabpanel = document.getElementById(
+        tab.getAttribute("aria-controls")
+      );
+
+      tab.tabIndex = -1;
+      tab.setAttribute("aria-selected", "false");
+      this.tabpanels.push(tabpanel);
+
+      tab.addEventListener("keydown", this.onKeydown.bind(this));
+      tab.addEventListener("click", this.onClick.bind(this));
+
+      if (!this.firstTab) {
+        this.firstTab = tab;
+      }
+      this.lastTab = tab;
+    }
+
+    this.setSelectedTab(this.firstTab);
+  }
+
+  setSelectedTab(currentTab) {
+
+    for (let i = 0; i < this.tabs.length; i += 1) {
+      const tab = this.tabs[i];
+      this.tabpanels[i].querySelectorAll('.mark').forEach(e => e.remove())
+      if (currentTab === tab) {
+        tab.setAttribute("aria-selected", "true");
+        tab.removeAttribute("tabindex");
+        const mark = document.createElement('em')
+        mark.classList.add('mark')
+        mark.textContent=' 🚩'
+        mark.style.color = 'transparent'
+        this.tabpanels[i].querySelector("h2").appendChild(mark)
+        this.tabpanels[i].style.removeProperty('zIndex')
+        this.tabpanels[i].style.zIndex = "0"
+      } else {
+        tab.setAttribute("aria-selected", "false");
+        tab.tabIndex = -1;
+        this.tabpanels[i].style.zIndex = "-1"
+      }
+
+    }
+
+  }
+
+  moveFocusToTab(currentTab) {
+    currentTab.focus();
+  }
+
+  moveFocusToPreviousTab(currentTab) {
+    let index;
+
+    if (currentTab === this.firstTab) {
+      this.moveFocusToTab(this.lastTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.moveFocusToTab(this.tabs[index - 1]);
+    }
+  }
+
+  moveFocusToNextTab(currentTab) {
+    let index;
+
+    if (currentTab === this.lastTab) {
+      this.moveFocusToTab(this.firstTab);
+    } else {
+      index = this.tabs.indexOf(currentTab);
+      this.moveFocusToTab(this.tabs[index + 1]);
+    }
+  }
+
+  /* EVENT HANDLERS */
+
+  onKeydown(event) {
+    const tgt = event.currentTarget;
+    let flag = false;
+
+    switch (event.key) {
+      case "ArrowLeft":
+        this.moveFocusToPreviousTab(tgt);
+        flag = true;
+        break;
+
+      case "ArrowRight":
+        this.moveFocusToNextTab(tgt);
+        flag = true;
+        break;
+
+      case "Home":
+        this.moveFocusToTab(this.firstTab);
+        flag = true;
+        break;
+
+      case "End":
+        this.moveFocusToTab(this.lastTab);
+        flag = true;
+        break;
+
+      default:
+        break;
+    }
+
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+  // Since this example uses buttons for the tabs, the click onr also is activated
+  // with the space and enter keys
+  onClick(event) {
+    this.setSelectedTab(event.currentTarget);
+    this.sel.removeAllRanges()
+    this.sel.addRange(this.range)
+    this.sel.removeRange(this.range)
+    console.log(this.sel.rangeCount);
+  }
+}
+
+// Initialize tablist
+
+window.addEventListener("load", function () {
+  const tablists = document.querySelectorAll("[role=tablist].manual");
+  for (let i = 0; i < tablists.length; i++) {
+    new TabsManual(tablists[i]);
+  }
+});
