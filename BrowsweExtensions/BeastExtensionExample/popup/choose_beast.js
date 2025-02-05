3fa6f9b8d918704ec01c7522365c4a9e170dfb35
thuypt <thuypt@reactplus.com>
2025-02-05 17:28:21 +0700

    Browser Extension - Option UI

--- BrowsweExtensions/BeastExtensionExample/popup/choose_beast.js
+++ BrowsweExtensions/BeastExtensionExample/popup/choose_beast.js
@@ -93,7 +93,7 @@ function listenForClicks() {
 function reportExecuteScriptError(error) {
   document.querySelector("#popup-content").classList.add("hidden");
   document.querySelector("#error-content").classList.remove("hidden");
-  console.error(`Failed to execute beastify content script: ${error}`);
+  // console.error(`Failed to execute beastify content script: ${error}`);
 }
 
 /**
